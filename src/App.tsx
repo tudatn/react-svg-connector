1fddeab4a7a666cb58726b99db9c41123ffa480b
Tu Dat Nguyen <tudat@affinitybridge.com>
2021-02-15 10:34:36 -0800

    Setup App

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import Draggable from "react-draggable";
+import styled from "styled-components/macro";
+import Connector from "./Connector";
+
+const Wrapper = styled.div`
+  position: relative;
+  height: 100vh;
+`;
+
+const Box = styled.div`
+  width: 150px;
+  height: 50px;
+  cursor: pointer;
+`;
+
+const Box1 = styled(Box)`
+  background-color: green;
+`;
+
+const Box2 = styled(Box)`
+  background-color: red;
+`;
 
 function App() {
+  const [activeDrags, setActiveDrags] = useState(0);
+  const [redraw, setRedraw] = useState(0);
+
+  const refs = useRef<{ [key: string]: any }>({});
+
+  function addRef(key: string) {
+    if (refs.current[key]) {
+      return refs.current[key];
+    } else {
+      const newRef = React.createRef<any>();
+      refs.current[key] = newRef;
+      return newRef;
+    }
+  }
+
+  function onStart() {
+    const newActiveDrags = activeDrags + 1;
+    setActiveDrags(newActiveDrags);
+  }
+
+  function onStop() {
+    const newActiveDrags = activeDrags - 1;
+    setActiveDrags(newActiveDrags);
+  }
+
+  useEffect(() => {
+    setRedraw(Math.random());
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Connector
+        el1={refs.current["box1"]?.current}
+        el2={refs.current["box2"]?.current}
+        shape="narrow-s"
+      />
+      <Draggable onStart={onStart} onStop={onStop}>
+        <Box1 ref={addRef("box1")} />
+      </Draggable>
+      <Draggable onStart={onStart} onStop={onStop}>
+        <Box2 ref={addRef("box2")} />
+      </Draggable>
+    </Wrapper>
   );
 }
 

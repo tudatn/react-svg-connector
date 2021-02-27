e65230c18b18e034f45f4812ff73dcb5f756cd8e
Tu Dat Nguyen <tudat@affinitybridge.com>
2021-02-27 09:22:32 -0800

    Update example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -9,10 +9,20 @@ const Wrapper = styled.div`
   overflow: auto;
 `;
 
-const Box = styled.div`
-  width: 150px;
+const Box = styled.div<{
+  top?: string;
+  left?: string;
+  right?: string;
+  bottom?: string;
+}>`
+  width: 100px;
   height: 50px;
   cursor: pointer;
+  position: absolute;
+  top: ${(props) => props.top || 0};
+  bottom: ${(props) => props.bottom || 0};
+  left: ${(props) => props.left || 0};
+  right: ${(props) => props.right || 0};
 `;
 
 const Box1 = styled(Box)`
@@ -66,12 +76,114 @@ function App() {
         direction="r2l"
         roundCorner={true}
       />
+      <Connector
+        el1={refs.current["box3"]?.current}
+        el2={refs.current["box4"]?.current}
+        shape="narrow-s"
+        direction="l2l"
+        stem={10}
+        roundCorner={true}
+      />
+      <Connector
+        el1={refs.current["box5"]?.current}
+        el2={refs.current["box6"]?.current}
+        shape="narrow-s"
+        direction="l2r"
+        stem={10}
+        roundCorner={true}
+      />
+      <Connector
+        el1={refs.current["box7"]?.current}
+        el2={refs.current["box8"]?.current}
+        shape="narrow-s"
+        direction="r2r"
+        stem={10}
+        roundCorner={true}
+      />
+
+      {/* YSPACE */}
+      <Connector
+        el1={refs.current["box9"]?.current}
+        el2={refs.current["box10"]?.current}
+        shape="narrow-s"
+        direction="t2b"
+        stem={10}
+        roundCorner={true}
+      />
+      <Connector
+        el1={refs.current["box11"]?.current}
+        el2={refs.current["box12"]?.current}
+        shape="narrow-s"
+        direction="t2t"
+        stem={10}
+        roundCorner={true}
+      />
+      <Connector
+        el1={refs.current["box13"]?.current}
+        el2={refs.current["box14"]?.current}
+        shape="narrow-s"
+        direction="b2t"
+        stem={10}
+        roundCorner={true}
+      />
+      <Connector
+        el1={refs.current["box15"]?.current}
+        el2={refs.current["box16"]?.current}
+        shape="narrow-s"
+        direction="b2b"
+        stem={10}
+        roundCorner={true}
+      />
 
       <Draggable onStart={onStart} onStop={onStop} onDrag={onDrag}>
-        <Box1 ref={addRef("box1")} />
+        <Box1 ref={addRef("box1")} top="150px" left="150px" />
       </Draggable>
       <Draggable onStart={onStart} onStop={onStop} onDrag={onDrag}>
-        <Box2 ref={addRef("box2")} />
+        <Box2 ref={addRef("box2")} top="300px" left="300px" />
+      </Draggable>
+      <Draggable onStart={onStart} onStop={onStop} onDrag={onDrag}>
+        <Box1 ref={addRef("box3")} top="150px" left="500px" />
+      </Draggable>
+      <Draggable onStart={onStart} onStop={onStop} onDrag={onDrag}>
+        <Box2 ref={addRef("box4")} top="300px" left="650px" />
+      </Draggable>
+      <Draggable onStart={onStart} onStop={onStop} onDrag={onDrag}>
+        <Box1 ref={addRef("box5")} top="150px" left="850px" />
+      </Draggable>
+      <Draggable onStart={onStart} onStop={onStop} onDrag={onDrag}>
+        <Box2 ref={addRef("box6")} top="300px" left="1000px" />
+      </Draggable>
+      <Draggable onStart={onStart} onStop={onStop} onDrag={onDrag}>
+        <Box1 ref={addRef("box7")} top="150px" left="1150px" />
+      </Draggable>
+      <Draggable onStart={onStart} onStop={onStop} onDrag={onDrag}>
+        <Box2 ref={addRef("box8")} top="300px" left="1300px" />
+      </Draggable>
+
+      {/* Y SPACE */}
+      <Draggable onStart={onStart} onStop={onStop} onDrag={onDrag}>
+        <Box1 ref={addRef("box9")} top="400px" left="150px" />
+      </Draggable>
+      <Draggable onStart={onStart} onStop={onStop} onDrag={onDrag}>
+        <Box2 ref={addRef("box10")} top="550px" left="300px" />
+      </Draggable>
+      <Draggable onStart={onStart} onStop={onStop} onDrag={onDrag}>
+        <Box1 ref={addRef("box11")} top="400px" left="500px" />
+      </Draggable>
+      <Draggable onStart={onStart} onStop={onStop} onDrag={onDrag}>
+        <Box2 ref={addRef("box12")} top="550px" left="650px" />
+      </Draggable>
+      <Draggable onStart={onStart} onStop={onStop} onDrag={onDrag}>
+        <Box1 ref={addRef("box13")} top="400px" left="850px" />
+      </Draggable>
+      <Draggable onStart={onStart} onStop={onStop} onDrag={onDrag}>
+        <Box2 ref={addRef("box14")} top="550px" left="1000px" />
+      </Draggable>
+      <Draggable onStart={onStart} onStop={onStop} onDrag={onDrag}>
+        <Box1 ref={addRef("box15")} top="400px" left="1150px" />
+      </Draggable>
+      <Draggable onStart={onStart} onStop={onStop} onDrag={onDrag}>
+        <Box2 ref={addRef("box16")} top="550px" left="1300px" />
       </Draggable>
     </Wrapper>
   );
